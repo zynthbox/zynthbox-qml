4869a8ff022bcac2ab4fee77798c3fe6b84e3c5c
Marco Martin <notmart@gmail.com>
2021-08-26 16:01:59 +0200

    iset path with qproperty assignment

--- zynqtgui/zynthiloops/libzl/zynthiloops_clip.py
+++ zynqtgui/zynthiloops/libzl/zynthiloops_clip.py
@@ -74,7 +74,7 @@ class zynthiloops_clip(QObject):
         #if "name" in obj:
             #self.name = obj["name"]
         if "path" in obj:
-            self.set_path(obj["path"])
+            self.path = obj["path"]
         if "start" in obj:
             self.__start_position__ = obj["start"]
         if "length" in obj:

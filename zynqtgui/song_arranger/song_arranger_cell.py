4b339f1f14675568ed4ebaece7f71f7a1b2860e8
Anupam Basak <anupam.basak27@gmail.com>
2021-09-20 17:21:06 +0530

    Add zlClip to grid cell object

--- zynqtgui/song_arranger/song_arranger_cell.py
+++ zynqtgui/song_arranger/song_arranger_cell.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import Property, QObject
+from PySide2.QtCore import Property, QObject, Signal
 
 
 class song_arranger_cell(QObject):
@@ -6,9 +6,20 @@ class song_arranger_cell(QObject):
         super(song_arranger_cell, self).__init__(parent)
 
         self.__id__ = id
+        self.__zl_clip__ = None
 
     ### Property id
     def get_id(self):
         return self.__id__
     id = Property(int, get_id, constant=True)
     ### END Property id
+
+    ### Property zlClip
+    def get_zl_clip(self):
+        return self.__zl_clip__
+    def set_zl_clip(self, clip):
+        self.__zl_clip__ = clip
+        self.zl_clip_changed.emit()
+    zl_clip_changed = Signal()
+    zlClip = Property(QObject, get_zl_clip, set_zl_clip, notify=zl_clip_changed)
+    ### END Property zlClip

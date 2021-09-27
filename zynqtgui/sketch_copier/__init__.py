83736a3361e1e5e653c5bea7d334d1c190418583
Anupam Basak <anupam.basak27@gmail.com>
2021-09-27 20:19:28 +0530

    Initial implementation of session management and sketch copier :   - Implement session serialization, saving and restore   - Implement creating a session json and save a song to slot 0 when a new sketch is saved   - Create a Sketch Copier page   - Load the session sketches   - Display loaded sketches name and tracks when available

---
+++ zynqtgui/sketch_copier/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__all__ = [
+    "zynthian_gui_sketch_copier",
+]
+
+from zynqtgui.sketch_copier.zynthian_gui_sketch_copier import (
+    zynthian_gui_sketch_copier,
+)
+
--- zynqtgui/zynthian_gui_session_dashboard.py
+++ zynqtgui/zynthian_gui_session_dashboard.py
@@ -6,6 +6,7 @@
 # Zynthian GUI Info Class
 # 
 # Copyright (C) 2021 Marco MArtin <mart@kde.org>
+# Copyright (C) 2021 Anupam Basak <anupam.basak27@gmail.com>
 #
 #******************************************************************************
 # 
@@ -22,17 +23,19 @@
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 # 
 #******************************************************************************
-
+import json
 import sys
 import logging
 
 from datetime import datetime
 
 # Zynthian specific modules
+from pathlib import Path
+
 from . import zynthian_gui_selector
 
 # Qt modules
-from PySide2.QtCore import Qt, QObject, Slot, Signal, Property
+from PySide2.QtCore import QTimer, Qt, QObject, Slot, Signal, Property
 
 
 #------------------------------------------------------------------------------
@@ -44,8 +47,94 @@ class zynthian_gui_session_dashboard(zynthian_gui_selector):
     def __init__(self, parent=None):
         super(zynthian_gui_session_dashboard, self).__init__('Session', parent)
         self.__sessionStartTime = datetime.now()
+        self.__sessions_base_dir__ = Path("/zynthian/zynthian-my-data/sessions/")
+        self.__save_timer__ = QTimer(self)
+
+        if not self.restore():
+            self.__name__ = None
+            self.__id__ = 0
+            self.__sketches__ = {}
+
+        self.__save_timer__.setInterval(1000)
+        self.__save_timer__.setSingleShot(True)
+        self.__save_timer__.timeout.connect(self.save)
 
         self.show()
+
+    ### Property name
+    def get_name(self):
+        if self.__name__ is not None:
+            return self.__name__
+        else:
+            return f"Session {self.__id__ + 1}"
+    def set_name(self, name):
+        self.__name__ = name
+        self.name_changed.emit()
+    name_changed = Signal()
+    name = Property(str, get_name, set_name, notify=name_changed)
+    ### END Property name
+
+    ### Property id
+    def get_id(self):
+        return self.__id__
+    id_changed = Signal()
+    id = Property(int, get_id, notify=id_changed)
+    ### END Property name
+
+    ### Property sketches
+    def get_sketches(self):
+        return self.__sketches__
+    def set_sketch(self, sketch, slot=0):
+        logging.error(f"Setting session sketch slot[{slot}] : {sketch}")
+
+        self.__sketches__[slot] = sketch
+        self.sketches_changed.emit()
+        self.schedule_save()
+    sketches_changed = Signal()
+    sketches = Property('QVariantMap', get_sketches, notify=sketches_changed)
+    ### END Property sketches
+
+    def serialize(self):
+        return {
+            "name": self.__name__,
+            "id": self.__id__,
+            "sketches": self.__sketches__
+        }
+
+    def schedule_save(self):
+        self.__save_timer__.start()
+
+    def save(self):
+        session_json_path = self.__sessions_base_dir__ / "session.json"
+        logging.error(f"Saving session : {session_json_path}")
+
+        self.__sessions_base_dir__.mkdir(parents=True, exist_ok=True)
+
+        with open(session_json_path, "w") as f:
+            json.dump(self.serialize(), f)
+
+    def restore(self):
+        session_json_path = self.__sessions_base_dir__ / "session.json"
+
+        try:
+            logging.error(f"Restoring session : {session_json_path}")
+            with open(session_json_path, "r") as f:
+                session = json.load(f)
+
+            if "name" in session:
+                self.__name__ = session["name"]
+                self.name_changed.emit()
+            if "id" in session:
+                self.__id__ = session["id"]
+                self.id_changed.emit()
+            if "sketches" in session:
+                self.__sketches__ = session["sketches"]
+                self.sketches_changed.emit()
+
+            return True
+        except Exception as e:
+            logging.error(f"Error restoring session({session_json_path}) : {str(e)}")
+            return False
 
     def fill_list(self):
         self.list_data = []

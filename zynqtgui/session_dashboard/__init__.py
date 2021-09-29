b120bc7b4181083540876dc7faba14d9c90864d1
Anupam Basak <anupam.basak27@gmail.com>
2021-09-29 13:37:00 +0530

    Move session dashboard to seperate dir

---
+++ zynqtgui/session_dashboard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__all__ = [
+    "zynthian_gui_session_dashboard",
+]
+
+from zynqtgui.session_dashboard.zynthian_gui_session_dashboard import (
+    zynthian_gui_session_dashboard,
+)
+

a4bc71b76b6a61090ca7296bebeda9bd96e5bbd9
Anupam Basak <anupam.basak27@gmail.com>
2021-10-18 12:47:36 +0530

    - Disable back action in looper page - Add Scenes Bar - Create scenes model - Display scenes buttons in scenes bar

---
+++ qml-ui/pages/ZynthiLoops/ScenesBar.qml
@@ -0,0 +1,67 @@
+/* -*- coding: utf-8 -*-
+******************************************************************************
+ZYNTHIAN PROJECT: Zynthian Qt GUI
+
+Zynthian Loopgrid Page
+
+Copyright (C) 2021 Anupam Basak <anupam.basak27@gmail.com>
+Copyright (C) 2021 Marco MArtin <mart@kde.org>
+
+******************************************************************************
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+For a full copy of the GNU General Public License see the LICENSE.txt file.
+
+******************************************************************************
+*/
+
+import QtQuick 2.10
+import QtQuick.Layouts 1.4
+import QtQuick.Controls 2.2 as QQC2
+import org.kde.kirigami 2.4 as Kirigami
+
+//import Zynthian 1.0 as Zynthian
+
+Rectangle {
+    Layout.fillWidth: true
+    color: Kirigami.Theme.backgroundColor
+
+    ColumnLayout {
+        anchors.fill: parent
+        anchors.centerIn: parent
+        anchors.margins: Kirigami.Units.gridUnit*0.3
+
+        Kirigami.Heading {
+            text: qsTr("Mixer : %1").arg(song.name)
+        }
+
+        GridLayout {
+            readonly property QtObject song: zynthian.zynthiloops.song
+
+            id: root
+            Layout.fillWidth: true
+            Layout.fillHeight: true
+            rows: 2
+            columns: Math.ceil(song.scenesModel.count/2)
+
+            Repeater {
+                model: song.scenesModel
+                delegate: QQC2.Button {
+                    Layout.fillWidth: true
+                    Layout.fillHeight: true
+
+                    text: model.name
+                }
+            }
+        }
+    }
+}
---
+++ zynqtgui/zynthiloops/libzl/zynthiloops_scenes_model.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ******************************************************************************
+# ZYNTHIAN PROJECT: Zynthian GUI
+#
+# A model to store scenes of a song in ZynthiLoops
+#
+# Copyright (C) 2021 Anupam Basak <anupam.basak27@gmail.com>
+#
+# ******************************************************************************
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# For a full copy of the GNU General Public License see the LICENSE.txt file.
+#
+# ******************************************************************************
+import logging
+from PySide2.QtCore import QAbstractListModel, QModelIndex, Qt, Property, Signal, Slot, QObject
+
+
+class zynthiloops_scenes_model(QAbstractListModel):
+    SceneRole = Qt.UserRole + 1
+    NameRole = SceneRole + 1
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.__scenes__ = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: [],
+            10: [],
+            11: [],
+        }
+
+    def serialize(self):
+        pass
+
+    def deserialize(self, arr):
+        pass
+
+    def data(self, index, role=None):
+        if not index.isValid():
+            return None
+
+        if index.row() > len(self.__scenes__):
+            return None
+
+        if role == self.SceneRole:
+            return self.__scenes__[index.row()]
+        elif role == Qt.DisplayRole or role == self.NameRole:
+            return chr(index.row() + 65)
+        else:
+            return None
+
+    def roleNames(self):
+        role_names = {
+            Qt.DisplayRole: b'display',
+            self.SceneRole: b"scene",
+            self.NameRole: b"name",
+        }
+
+        return role_names
+
+    def rowCount(self, index):
+        return len(self.__scenes__)
+
+    ### Property count
+    def count(self):
+        return len(self.__scenes__)
+    countChanged = Signal()
+    count = Property(int, count, notify=countChanged)
+    ### END Property count

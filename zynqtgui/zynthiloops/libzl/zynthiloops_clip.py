419552eabe295fa41a3cdcf4016221be7df6e243
Marco Martin <notmart@gmail.com>
2021-12-15 16:32:03 +0000

    new titles on patterns

--- zynqtgui/zynthiloops/libzl/zynthiloops_clip.py
+++ zynqtgui/zynthiloops/libzl/zynthiloops_clip.py
@@ -84,6 +84,12 @@ class zynthiloops_clip(QObject):
     initialStartPosition = Property(float, get_initial_start_position, constant=True)
     ### END Property initialStartPosition
 
+    ### Property initialStartPosition
+    def get_track(self):
+        return self.track
+    clipTrack = Property(QObject, get_track, constant=True)
+    ### END Property initialStartPosition
+
     ### Property initialLength
     def get_initial_length(self):
         return self.__initial_length__

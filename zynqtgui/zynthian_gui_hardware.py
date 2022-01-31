6743a8260174281310633ae720b6b45ccc8470d5
Anupam Basak <anupam.basak27@gmail.com>
2022-01-31 19:43:00 +0530

    - Add stop button to kill audio/midi process - Do not show info dialog as it is very wonky

--- zynqtgui/zynthian_gui_hardware.py
+++ zynqtgui/zynthian_gui_hardware.py
@@ -54,8 +54,11 @@ class zynthian_gui_hardware(zynthian_gui_selector):
 
     def fill_list(self):
         self.list_data = []
-        self.list_data.append((self.test_audio, 0, "Test Audio"))
-        self.list_data.append((self.test_midi, 0, "Test MIDI"))
+
+        self.list_data.append((self.test_audio, 0, "Start Test Audio"))
+        self.list_data.append((self.kill_command, 0, "Stop Test Audio"))
+        self.list_data.append((self.test_midi, 0, "Start Test MIDI"))
+        self.list_data.append((self.kill_command, 0, "Stop Test MIDI"))
         self.list_data.append((self.test_touchpoints, 0, "Test Touchpoints"))
         self.list_data.append(
             (self.zyngui.calibrate_touchscreen, 0, "Calibrate Touchscreen")
@@ -73,13 +76,13 @@ class zynthian_gui_hardware(zynthian_gui_selector):
         super().set_select_path()
 
     def execute_commands(self):
-        self.zyngui.start_loading()
+        # self.zyngui.start_loading()
 
         error_counter = 0
         for cmd in self.commands:
             logging.info("Executing Command: %s" % cmd)
-            self.zyngui.add_info("EXECUTING:\n", "EMPHASIS")
-            self.zyngui.add_info("{}\n".format(cmd))
+            # self.zyngui.add_info("EXECUTING:\n", "EMPHASIS")
+            # self.zyngui.add_info("{}\n".format(cmd))
             try:
                 self.proc = Popen(
                     cmd,
@@ -88,7 +91,7 @@ class zynthian_gui_hardware(zynthian_gui_selector):
                     stderr=STDOUT,
                     universal_newlines=True,
                 )
-                self.zyngui.add_info("RESULT:\n", "EMPHASIS")
+                # self.zyngui.add_info("RESULT:\n", "EMPHASIS")
                 for line in self.proc.stdout:
                     if re.search("ERROR", line, re.IGNORECASE):
                         error_counter += 1
@@ -98,25 +101,25 @@ class zynthian_gui_hardware(zynthian_gui_selector):
                     else:
                         tag = None
                     logging.info(line.rstrip())
-                    self.zyngui.add_info(line, tag)
-                self.zyngui.add_info("\n")
+                    # self.zyngui.add_info(line, tag)
+                # self.zyngui.add_info("\n")
             except Exception as e:
                 logging.error(e)
-                self.zyngui.add_info("ERROR: %s\n" % e, "ERROR")
+                # self.zyngui.add_info("ERROR: %s\n" % e, "ERROR")
 
         if error_counter > 0:
             logging.info("COMPLETED WITH {} ERRORS!".format(error_counter))
-            self.zyngui.add_info(
-                "COMPLETED WITH {} ERRORS!".format(error_counter), "WARNING"
-            )
+            # self.zyngui.add_info(
+            #     "COMPLETED WITH {} ERRORS!".format(error_counter), "WARNING"
+            # )
         else:
             logging.info("COMPLETED OK!")
-            self.zyngui.add_info("COMPLETED OK!", "SUCCESS")
+            # self.zyngui.add_info("COMPLETED OK!", "SUCCESS")
 
         self.commands = None
-        self.zyngui.add_info("\n\n")
-        self.zyngui.hide_info_timer(5000)
-        self.zyngui.stop_loading()
+        # self.zyngui.add_info("\n\n")
+        # self.zyngui.hide_info_timer(5000)
+        # self.zyngui.stop_loading()
 
     def start_command(self, cmds):
         if not self.commands:
@@ -130,28 +133,28 @@ class zynthian_gui_hardware(zynthian_gui_selector):
         # self.zyngui.start_loading()
         for cmd in self.commands:
             logging.info("Executing Command: %s" % cmd)
-            self.zyngui.add_info("EXECUTING:\n", "EMPHASIS")
-            self.zyngui.add_info("{}\n".format(cmd))
+            # self.zyngui.add_info("EXECUTING:\n", "EMPHASIS")
+            # self.zyngui.add_info("{}\n".format(cmd))
             try:
                 proc = Popen(cmd.split(" "), stdout=PIPE, stderr=PIPE)
                 self.child_pid = proc.pid
-                self.zyngui.add_info("\nPID: %s" % self.child_pid)
+                # self.zyngui.add_info("\nPID: %s" % self.child_pid)
                 (output, error) = proc.communicate()
                 self.child_pid = None
                 if error:
                     result = "ERROR: %s" % error
                     logging.error(result)
-                    self.zyngui.add_info(result, "ERROR")
+                    # self.zyngui.add_info(result, "ERROR")
                 if output:
                     logging.info(output)
-                    self.zyngui.add_info(output)
+                    # self.zyngui.add_info(output)
             except Exception as e:
                 result = "ERROR: %s" % e
                 logging.error(result)
-                self.zyngui.add_info(result, "ERROR")
+                # self.zyngui.add_info(result, "ERROR")
 
         self.commands = None
-        self.zyngui.hide_info_timer(5000)
+        # self.zyngui.hide_info_timer(5000)
         # self.zyngui.stop_loading()
 
     def killable_start_command(self, cmds):
@@ -182,7 +185,7 @@ class zynthian_gui_hardware(zynthian_gui_selector):
 
     def test_audio(self):
         logging.info("TESTING AUDIO")
-        self.zyngui.show_info("TEST AUDIO")
+        # self.zyngui.show_info("TEST AUDIO")
         # self.killable_start_command(["mpg123 {}/audio/test.mp3".format(self.data_dir)])
         self.killable_start_command(
             [
@@ -196,7 +199,7 @@ class zynthian_gui_hardware(zynthian_gui_selector):
 
     def test_midi(self):
         logging.info("TESTING MIDI")
-        self.zyngui.show_info("TEST MIDI")
+        # self.zyngui.show_info("TEST MIDI")
         self.killable_start_command(
             ["aplaymidi -p 14 {}/mid/test.mid".format(self.data_dir)]
         )

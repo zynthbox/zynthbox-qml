9bf83c5cce63a6c181572c7056bdbea84dee8f35
Anupam Basak <anupam.basak27@gmail.com>
2022-02-08 18:09:42 +0530

    Dispaly soundcard name and available capture channels

--- zynqtgui/zynthian_gui_audio_settings.py
+++ zynqtgui/zynthian_gui_audio_settings.py
@@ -23,7 +23,10 @@
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 #
 # ******************************************************************************
+import os
 
+import jack
+from PySide2.QtCore import Property
 
 from . import zynthian_qt_gui_base
 
@@ -40,3 +43,19 @@ class zynthian_gui_audio_settings(zynthian_qt_gui_base.ZynGui):
 
     def refresh_loading(self):
         pass
+
+    ### Property soundcardName
+    def get_soundcard_name(self):
+        return os.environ.get("SOUNDCARD_NAME", "Unknown")
+    soundcardName = Property(str, get_soundcard_name, constant=True)
+    ### END Property soundcardName
+
+    ### Property channels
+    def get_channels(self):
+        _channels = []
+        for port in jack.Client('').get_ports("system:capture"):
+            _channels.append(port.name.replace("system:", ""))
+
+        return _channels
+    channels = Property('QVariantList', get_channels, constant=True)
+    ### END Property channels

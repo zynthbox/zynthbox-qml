8c4a26a5dd8f5f4fadb7eb9c0f14dec3f64c4631
Anupam Basak <anupam.basak27@gmail.com>
2022-03-09 18:12:19 +0530

    - Update samples dir location to reside under wav dir - Update sampleset.json to store only path and restore from only path where other properties are restored from metadata - Add missing metadata properties for loopDelta and snapLengthToBeat

--- zynqtgui/zynthiloops/libzl/zynthiloops_song.py
+++ zynqtgui/zynthiloops/libzl/zynthiloops_song.py
@@ -90,8 +90,8 @@ class zynthiloops_song(QObject):
 
         # Create wav dir for recording
         (Path(self.sketch_folder) / 'wav').mkdir(parents=True, exist_ok=True)
-        # Create root samples dir if not exists
-        (Path(self.sketch_folder) / 'samples').mkdir(parents=True, exist_ok=True)
+        # Create samples dir if not exists
+        (Path(self.sketch_folder) / 'wav' / 'samples').mkdir(parents=True, exist_ok=True)
 
     def to_be_deleted(self):
         self.__to_be_deleted__ = True

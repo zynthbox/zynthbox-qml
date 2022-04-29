a677b456bee1c89fcb794aed2ea93cac4769d746
Anupam Basak <anupam.basak27@gmail.com>
2022-04-29 18:03:06 +0530

    Fix wrong check

--- zynqtgui/zynthiloops/libzl/zynthiloops_track.py
+++ zynqtgui/zynthiloops/libzl/zynthiloops_track.py
@@ -101,7 +101,7 @@ class zynthiloops_track(QObject):
         self.occupiedSlotsChanged.emit()
 
         # Update zynthiloops recorder ports if selected track chained sounds changes
-        if self.zyngui.selectedTrack == self.__id__:
+        if self.zyngui.session_dashboard.selectedTrack == self.__id__:
             self.zyngui.zynthiloops.update_recorder_jack_port()
 
     @Property(str, constant=True)

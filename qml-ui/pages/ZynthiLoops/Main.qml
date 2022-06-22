zynthbox/zynthbox-qml

commit defb5bbbe078386f85d9e9f4f4416d0eea02d442

delay more bindings

qml-ui/pages/ZynthiLoops/Main.qml
@@ -948,6 +948,7 @@ Zynthian.ScreenPage {
                         model: zynthian.isBootingComplete ? root.song.tracksModel : 0
 
                         delegate: TrackHeader2 {
+                            id: delegateTrackHeader2
                             text: model.track.name
 
                             Connections {
@@ -1018,21 +1019,28 @@ Zynthian.ScreenPage {
 
                             subSubTextSize: 7
 
-                            color: {
-                                if (root.copySourceObj === model.track)
-                                    return "#ff2196f3"
-                                else if (model.track.trackAudioType === "synth" && model.track.occupiedSlotsCount > 0)
-                                    return "#66ff0000"
-                                else if (model.track.trackAudioType === "sample-loop" && model.track.sceneClip.path && model.track.sceneClip.path.length > 0)
-                                    return "#6600ff00"
-                                else if (model.track.trackAudioType === "sample-trig" && model.track.occupiedSlotsCount > 0)
-                                    return "#66ffff00"
-                                else if (model.track.trackAudioType === "sample-slice" && model.track.occupiedSlotsCount > 0)
-                                    return "#66ffff00"
-                                else if (model.track.trackAudioType === "external")
-                                    return "#998e24aa"
-                                else
-                                    return "#66888888"
+                            Binding {
+                                target: delegateTrackHeader2
+                                property: "color"
+                                when: root.active
+                                delayed: true
+
+                                value: {
+                                    if (root.copySourceObj === model.track)
+                                        return "#ff2196f3"
+                                    else if (model.track.trackAudioType === "synth" && model.track.occupiedSlotsCount > 0)
+                                        return "#66ff0000"
+                                    else if (model.track.trackAudioType === "sample-loop" && model.track.sceneClip.path && model.track.sceneClip.path.length > 0)
+                                        return "#6600ff00"
+                                    else if (model.track.trackAudioType === "sample-trig" && model.track.occupiedSlotsCount > 0)
+                                        return "#66ffff00"
+                                    else if (model.track.trackAudioType === "sample-slice" && model.track.occupiedSlotsCount > 0)
+                                        return "#66ffff00"
+                                    else if (model.track.trackAudioType === "external")
+                                        return "#998e24aa"
+                                    else
+                                        return "#66888888"
+                                }
                             }
 
                             highlightOnFocus: false

commit a9c829d4391c33d1371f9ac2c53c476ea83305e4

ZL Main : defb5bb introduced a Binding but it was bound to a wrong property. Bind to visible instead of active

qml-ui/pages/ZynthiLoops/Main.qml
@@ -1027,7 +1027,7 @@ Zynthian.ScreenPage {
                             Binding {
                                 target: delegateTrackHeader2
                                 property: "color"
-                                when: root.active
+                                when: root.visible
                                 delayed: true
 
                                 value: {
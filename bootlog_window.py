e892e92900eb1ef5921a62a32f2e42e67695657f
Anupam Basak <anupam.basak27@gmail.com>
2022-07-20 17:13:10 +0530

    Update bootlog_window to expose a Property for display boot boot logs

--- bootlog_window.py
+++ bootlog_window.py
@@ -1,28 +1,68 @@
 import logging
-import select
+import os
 import sys
 from pathlib import Path
-from time import sleep
+from PySide2.QtCore import Property, QObject, QTimer, Signal
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
 
 
 logging.basicConfig(format='%(levelname)s:%(module)s.%(funcName)s: %(message)s', stream=sys.stderr, level=logging.DEBUG)
 
 
-if __name__ == "__main__":
-    while not Path("/tmp/bootlog.fifo").exists():
-        logging.debug("bootlog.fifo not found. Waiting")
-        sleep(1)
-        continue
+class BootLogInterface(QObject):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-    logging.debug("Found bootlog.fifo file. Reading.")
+        self.__boot_log = ""
+        self.__boot_log_file = None
 
-    with open("/tmp/bootlog.fifo", "r") as fifo:
-        while True:
-            data = fifo.readline()[:-1].strip()
+        self.check_boot_log_timer = QTimer()
+        self.check_boot_log_timer.setInterval(10)
+        self.check_boot_log_timer.setSingleShot(True)
+        self.check_boot_log_timer.timeout.connect(self.check_boot_log_timer_timeout)
+        self.check_boot_log_timer.start()
+
+    def check_boot_log_timer_timeout(self):
+        if not Path("/tmp/bootlog.fifo").exists():
+            logging.debug("bootlog.fifo not found. Waiting")
+        else:
+            if self.__boot_log_file is None:
+                self.__boot_log_file = open("/tmp/bootlog.fifo", "r")
+
+            data = self.__boot_log_file.readline()[:-1].strip()
 
             if data == "exit":
                 logging.debug("Received exit command. Cleaning up and exiting")
                 sys.exit(0)
-            else:
-                if len(data) > 0:
-                    logging.debug(data)
+            elif len(data) > 0:
+                self.bootLog = data
+
+        self.check_boot_log_timer.start()
+
+    ### Property bootLog
+    def get_bootLog(self):
+        return self.__boot_log
+
+    def set_bootLog(self, value):
+        if self.__boot_log != value:
+            logging.debug(f"Setting bootLog : {value}")
+            self.__boot_log = value
+            self.bootLogChanged.emit()
+
+    bootLogChanged = Signal()
+
+    bootLog = Property(str, get_bootLog, set_bootLog, notify=bootLogChanged)
+    ### END Property bootLog
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    bootLogInterface = BootLogInterface(app)
+
+    engine.rootContext().setContextProperty("bootLogInterface", bootLogInterface)
+    engine.load(os.fspath(Path(__file__).resolve().parent / "qml-ui/BootLogWindow.qml"))
+
+    sys.exit(app.exec_())
+

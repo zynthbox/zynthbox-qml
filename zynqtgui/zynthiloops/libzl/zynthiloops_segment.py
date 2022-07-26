1b6a1339e26522229728e79b83ade80624a213ec
Dan Leinir Turthra Jensen <admin@leinir.dk>
2022-07-26 14:50:46 +0100

    zl.segment: Fix missing bits from earlier refactoring

--- zynqtgui/zynthiloops/libzl/zynthiloops_segment.py
+++ zynqtgui/zynthiloops/libzl/zynthiloops_segment.py
@@ -209,7 +209,7 @@ class zynthiloops_segment(QObject):
 
             logging.debug(f"Adding clip(row: {clip.row}, col: {clip.col}) to segment {self.segmentId}")
             self.__clips.append(clip)
-            self.zyngui.zynthiloops.song.mixesModel.clipAdded.emit(self.__mix.mixId, self.__segment_id, clip)
+            self.zyngui.zynthiloops.song.mixesModel.clipAdded.emit(self.__mix.mixId, self.segmentId, clip)
             self.clipsChanged.emit()
 
             if self.zyngui.zynthiloops.song is not None:
@@ -224,7 +224,7 @@ class zynthiloops_segment(QObject):
         if clip in self.__clips:
             logging.debug(f"Removing clip(row: {clip.row}, col: {clip.col}) from segment {self.segmentId}")
             self.__clips.remove(clip)
-            self.zyngui.zynthiloops.song.mixesModel.clipRemoved.emit(self.__mix.mixId, self.__segment_id, clip)
+            self.zyngui.zynthiloops.song.mixesModel.clipRemoved.emit(self.__mix.mixId, self.segmentId, clip)
             self.clipsChanged.emit()
 
             if self.zyngui.zynthiloops.song is not None:

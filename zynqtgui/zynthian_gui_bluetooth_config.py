c5f9b6f4aec374f883cf0d8d4efb97153effd4fd
Dan Leinir Turthra Jensen <admin@leinir.dk>
2023-01-18 14:16:08 +0000

    bt config: Add auto-retry (tries five times when connecting)
    This works around the occasional situation where connecting to a bt
    audio device takes longer or fails the first time and the alsa device
    creation takes a little bit longer than usual. It's not quite perfect
    (probably wants some kind of throttle added so it's not just a hammer),
    but should work reasonably well at least.

--- zynqtgui/zynthian_gui_bluetooth_config.py
+++ zynqtgui/zynthian_gui_bluetooth_config.py
@@ -55,6 +55,16 @@ class zynthian_gui_bluetooth_config(zynthian_qt_gui_base.ZynGui):
         Please note that running zita-j2a process without connecting to a bluetooth device will result in a failure.
         Hence, make sure to run this method after connecting to a device
         """
+        self.connection_attempt_count = 0
+        def attempt_connection():
+            if self.zita_j2a_process is not None:
+                self.zita_j2a_process.terminate()
+            logging.info(f"Attempting to start zita-j2a bridge, attempt number {self.connection_attempt_count}")
+            self.zita_j2a_process = Popen(("zita-j2a", "-j", "bluealsa", "-d", "bluealsa", "-p", "1024", "-n", "3", "-c", "2", "-L"))
+            # Connect to bluealsa ports after a short timeout to allow zita-j2a to set up jack ports before
+            # attempting to connect otherwise it will end up failing to connect to the ports
+            QTimer.singleShot(3000, post_startup_task)
+
         @Slot()
         def post_startup_task():
             # If zita-j2a bridge is running do run autoconnect to connect to bluetooth ports
@@ -63,12 +73,9 @@ class zynthian_gui_bluetooth_config(zynthian_qt_gui_base.ZynGui):
                 self.zyngui.zynautoconnect()
             else:
                 logging.error("Failed to start zita-j2a bridge. Check if bluetooth device is connected")
+                if (self.connection_attempt_count < 5):
+                    self.connection_attempt_count = self.connection_attempt_count + 1
+                    attempt_connection()
 
-        if self.zita_j2a_process is not None:
-            self.zita_j2a_process.terminate()
+        attempt_connection()
 
-        self.zita_j2a_process = Popen(("zita-j2a", "-j", "bluealsa", "-d", "bluealsa", "-p", "1024", "-n", "3", "-c", "2", "-L"))
-
-        # Connect to bluealsa ports after a short timeout to allow zita-j2a to set up jack ports before
-        # attempting to connect otherwise it will end up failing to connect to the ports
-        QTimer.singleShot(3000, post_startup_task)

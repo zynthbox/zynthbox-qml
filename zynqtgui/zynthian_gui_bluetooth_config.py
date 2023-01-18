f06166b0954241a097d50ed601af38f5057ee9c3
Anupam Basak <anupam.basak27@gmail.com>
2023-01-18 14:02:53 +0530

    Bluetooth : Add method to connect to bluetooth ports when a device is connected

--- zynqtgui/zynthian_gui_bluetooth_config.py
+++ zynqtgui/zynthian_gui_bluetooth_config.py
@@ -22,7 +22,10 @@
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 #
 # ******************************************************************************
+import logging
+from subprocess import Popen
 
+from PySide2.QtCore import QTimer, Slot
 
 from . import zynthian_qt_gui_base
 
@@ -30,6 +33,7 @@ from . import zynthian_qt_gui_base
 class zynthian_gui_bluetooth_config(zynthian_qt_gui_base.ZynGui):
     def __init__(self, parent=None):
         super(zynthian_gui_bluetooth_config, self).__init__(parent)
+        self.zita_j2a_process = None
 
     def show(self):
         pass
@@ -39,3 +43,21 @@ class zynthian_gui_bluetooth_config(zynthian_qt_gui_base.ZynGui):
 
     def refresh_loading(self):
         pass
+
+    @Slot()
+    def connectBluetoothPorts(self):
+        @Slot()
+        def post_startup_task():
+            # If zita-j2a bridge is running do run autoconnect to connect to bluetooth ports
+            if self.zita_j2a_process is not None and self.zita_j2a_process.poll() is None:
+                logging.info("Successfully started zita-j2a bridge. Bluetooth device should play audio now")
+                self.zyngui.zynautoconnect()
+            else:
+                logging.error("Failed to start zita-j2a bridge. Check if bluetooth device is connected")
+
+        if self.zita_j2a_process is not None:
+            self.zita_j2a_process.terminate()
+
+        self.zita_j2a_process = Popen(("zita-j2a", "-j", "bluealsa", "-d", "bluealsa", "-p", "1024", "-n", "3", "-c", "2", "-L"))
+
+        QTimer.singleShot(3000, post_startup_task)

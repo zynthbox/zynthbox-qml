729f082d0e966428051db95a49e69381d2b6ecbc
Anupam Basak <anupam.basak27@gmail.com>
2023-09-15 14:09:33 +0530

    zynthian_controller : For learn CC stuff, send out cc notes to lear channel and learn cc

--- zyngine/zynthian_controller.py
+++ zyngine/zynthian_controller.py
@@ -301,7 +301,7 @@ class zynthian_controller(QObject):
             # Send feedback to MIDI controllers
             try:
                 if self.midi_learn_cc:
-                    Zynthbox.SyncTimer.instance().sendMidiMessageImmediately(3, 176 + self.midi_chan, self.midi_cc, mval)
+                    Zynthbox.SyncTimer.instance().sendMidiMessageImmediately(3, 176 + self.midi_learn_chan, self.midi_learn_cc, mval)
                     # zyncoder.lib_zyncoder.ctrlfb_send_ccontrol_change(self.midi_learn_chan,self.midi_learn_cc,mval)
                     #logging.debug("Controller feedback '{}' (learn) => CH{}, CC{}, Val={}".format(self.symbol,self.midi_learn_chan,self.midi_learn_cc,mval))
                 elif self.midi_cc:

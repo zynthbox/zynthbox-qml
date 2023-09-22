108c276e6cf0c83d8eb44fae043a8747c2d44302
Dan Leinir Turthra Jensen <admin@leinir.dk>
2023-09-22 10:14:11 +0100

    sketchpad.song: Fix muting (lane logic was incorrect, woops)

--- zynqtgui/sketchpad/sketchpad_song.py
+++ zynqtgui/sketchpad/sketchpad_song.py
@@ -642,13 +642,13 @@ class sketchpad_song(QObject):
                 channel = self.channelsModel.getChannel(channel_index)
                 if value == -1:
                     for laneId in range(0, 5):
-                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * laneId].setMuted(channel.muted)
+                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * 5 + laneId].setMuted(channel.muted)
                 elif value == channel.id:
                     for laneId in range(0, 5):
-                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * laneId].setMuted(False)
+                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * 5 + laneId].setMuted(False)
                 else:
                     for laneId in range(0, 5):
-                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * laneId].setMuted(True)
+                        Zynthbox.Plugin.instance().channelPassthroughClients()[channel.id * 5 + laneId].setMuted(True)
 
             self.playChannelSoloChanged.emit()
 

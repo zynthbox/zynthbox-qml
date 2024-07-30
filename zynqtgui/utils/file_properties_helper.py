1ebd8f1b972e21fce00501d8e54c4b55ff2e07cb
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-07-30 16:54:44 +0100

    file_properties_helper: Add in more info for sketches
    This is kind of just the beginning, we'll need considerably more here
    (like more generic tag stuff... as well as "feed our search db" stuff,
    which we'll need for the not-file-structure based searching stuff we'll
    want, but perhaps not just now)

--- zynqtgui/utils/file_properties_helper.py
+++ zynqtgui/utils/file_properties_helper.py
@@ -10,6 +10,13 @@ from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QApplication, QStyle
 from soundfile import SoundFile
 
+# Function by Fred Cirera - https://web.archive.org/web/20111010015624/http://blogmag.net/blog/read/38/Print_human_readable_file_size
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
 class file_properties_helper(QObject):
     def __init__(self, parent=None):
@@ -28,8 +35,10 @@ class file_properties_helper(QObject):
 
         is_wav = self.file_path.name.endswith(".wav")
 
+        is_sketch = False
         if is_wav:
             properties = self.getWavData(self.file_path)
+            is_sketch = "zynthbox" in properties
         else:
             properties = []
 
@@ -39,7 +48,9 @@ class file_properties_helper(QObject):
             "filepath": str(self.file_path),
             "filename": self.file_path.name,
             "size": file_stat.st_size,
+            "humanSize": sizeof_fmt(file_stat.st_size),
             "isWav": is_wav,
+            "isSketch": is_sketch,
             "isDir": self.file_path.is_dir(),
             "isFile": self.file_path.is_file(),
             "properties": properties
@@ -94,16 +105,75 @@ class file_properties_helper(QObject):
     isPreviewPlaying = Property(bool, get_is_preview_playing, notify=is_preview_playing_changed)
     ### END Property isPreviewPlaying
 
+
     @staticmethod
     def getWavData(path):
+        def getMetadataProperty(metadata, name, default=None):
+            try:
+                value = metadata[name][0]
+                if value == "None":
+                    # If 'None' value is saved, return default
+                    return default
+                return value
+            except:
+                return default
         try:
             f = SoundFile(path)
-            return {
-                "frames": f.frames,
-                "sampleRate": f.samplerate,
-                "channels": f.channels,
-                "duration": f.frames/f.samplerate
-            }
+            try:
+                file = taglib.File(path)
+                metadata = file.tags
+                file.close()
+            except Exception as e:
+                metadata = {}
+            if "ZYNTHBOX_BPM" in metadata:
+                snapshot = getMetadataProperty(metadata, "ZYNTHBOX_SOUND_SNAPSHOT", None)
+                soundDescriptions = []
+                if snapshot is not None:
+                    # This is so we don't have to import the layers code from parent, but... maybe we want to, or it wants to live elsewhere, or... something
+                    try:
+                        data = []
+                        layers = JSONDecoder().decode(snapshot)
+                        for layer_data in layers["layers"]:
+                            layer_snapshot = self.zynqtgui.zynthbox_plugins_helper.update_layer_snapshot_plugin_id_to_name(layer_data)
+                            #if not layer_snapshot["midi_chan"] in data:
+                                #data[layer_snapshot["midi_chan"]] = []
+                            item = {"name": layer_snapshot["engine_name"].split("/")[-1]}
+                            if "midi_chan" in layer_snapshot:
+                                item["midi_chan"] = layer_snapshot["midi_chan"]
+                            else:
+                                item["midi_chan"] = -1
+                            if "slot_index" in layer_snapshot:
+                                item["slot_index"] = layer_snapshot["slot_index"]
+                            if "bank_name" in layer_snapshot:
+                                item["bank_name"] = layer_snapshot["bank_name"]
+                            if "preset_name" in layer_snapshot:
+                                item["preset_name"] = layer_snapshot["preset_name"]
+                            if "engine_type" in layer_snapshot:
+                                item["engine_type"] = layer_snapshot["engine_type"]
+                            data.append(item)
+                        for entry in data:
+                            if entry["name"] != "":
+                                soundDescriptions.append(f"{entry['name']} ({entry['preset_name']})")
+                    except Exception as e:
+                        logging.error(e)
+                return {
+                    "frames": f.frames,
+                    "sampleRate": f.samplerate,
+                    "channels": f.channels,
+                    "duration": f.frames/f.samplerate,
+                    "zynthbox": {
+                        "bpm": getMetadataProperty(metadata, "ZYNTHBOX_BPM", 0),
+                        "playbackStyle": getMetadataProperty(metadata, "ZYNTHBOX_PLAYBACK_STYLE", "LoopingPlaybackStyle").split(".")[-1],
+                        "soundDescriptions": soundDescriptions
+                    }
+                }
+            else:
+                return {
+                    "frames": f.frames,
+                    "sampleRate": f.samplerate,
+                    "channels": f.channels,
+                    "duration": f.frames/f.samplerate
+                }
         except:
             return []
 

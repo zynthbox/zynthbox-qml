5c4649a3aee739da3745e379c09b8931b4fc53c4
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-11-11 11:08:53 +0000

    CommonUtils: Add a time formatter function (from song manager)

--- qml-ui/Zynthian/CommonUtils.js
+++ qml-ui/Zynthian/CommonUtils.js
@@ -118,3 +118,19 @@ function fitInWindow(originalX, windowStart, windowEnd) {
     let windowRatio = windowSize / 1;
     return movedX / windowRatio;
 }
+
+/**
+ * Return a pleasantly formatted time based on the given number of seconds
+ */
+function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    if (hours > 0) {
+        return `${hours}h${minutes}m${remainingSeconds}s`;
+    } else if (minutes > 0) {
+        return `${minutes}m${remainingSeconds}s`;
+    }
+    return `${seconds}s`;
+}

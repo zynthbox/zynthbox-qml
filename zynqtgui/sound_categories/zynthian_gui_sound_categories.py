db45e557427df4cd66d336ed649235e3b4d0d4af
Anupam Basak <anupam.basak27@gmail.com>
2025-03-20 17:50:52 +0530

    gui_sound_categories : Wrap saveSound in a do_long_task to display loading screen while file is being saved

--- zynqtgui/sound_categories/zynthian_gui_sound_categories.py
+++ zynqtgui/sound_categories/zynthian_gui_sound_categories.py
@@ -99,19 +99,23 @@ class zynthian_gui_sound_categories(zynthian_qt_gui_base.zynqtgui):
     def saveSound(self, name: str, category: int):
         if not name.endswith(".snd"):
             name = f"{name}.snd"
-        selectedTrack = self.zynqtgui.sketchpad.song.channelsModel.getChannel(self.zynqtgui.sketchpad.selectedTrackId)
-        sound = zynthbox_sndfile(self, self.zynqtgui, name, "my-sounds")
-        sound.metadata.synthFxSnapshot = selectedTrack.getChannelSoundSnapshot()
-        sound.metadata.sampleSnapshot = selectedTrack.getChannelSampleSnapshot()
-        sound.metadata.category = category
-        # selectedTrack.synthSlotsData is a list of strings (just what we need)
-        sound.metadata.synthSlotsData = json.dumps(selectedTrack.synthSlotsData)
-        # selectedTrack.sampleSlotsData is a list of clips (Map to a list of filenames)
-        sound.metadata.sampleSlotsData = json.dumps([Path(f.path if f.path is not None else "").name for f in selectedTrack.sampleSlotsData])
-        # selectedTrack.fxSlotsData is a list of strings (just what we need)
-        sound.metadata.fxSlotsData = json.dumps(selectedTrack.fxSlotsData)
-        sound.metadata.write()
-        Zynthbox.SndLibrary.instance().addSndFiles([sound.path], sound.type, f"/zynthian/zynthian-my-data/sounds/{sound.type}/.stat.json")
+        def task():
+            selectedTrack = self.zynqtgui.sketchpad.song.channelsModel.getChannel(self.zynqtgui.sketchpad.selectedTrackId)
+            sound = zynthbox_sndfile(self, self.zynqtgui, name, "my-sounds")
+            sound.metadata.synthFxSnapshot = selectedTrack.getChannelSoundSnapshot()
+            sound.metadata.sampleSnapshot = selectedTrack.getChannelSampleSnapshot()
+            sound.metadata.category = category
+            # selectedTrack.synthSlotsData is a list of strings (just what we need)
+            sound.metadata.synthSlotsData = json.dumps(selectedTrack.synthSlotsData)
+            # selectedTrack.sampleSlotsData is a list of clips (Map to a list of filenames)
+            sound.metadata.sampleSlotsData = json.dumps([Path(f.path if f.path is not None else "").name for f in selectedTrack.sampleSlotsData])
+            # selectedTrack.fxSlotsData is a list of strings (just what we need)
+            sound.metadata.fxSlotsData = json.dumps(selectedTrack.fxSlotsData)
+            sound.metadata.write()
+            Zynthbox.SndLibrary.instance().addSndFiles([sound.path], sound.type, f"/zynthian/zynthian-my-data/sounds/{sound.type}/.stat.json")
+            self.zynqtgui.end_long_task()
+        self.zynqtgui.do_long_task(task, "Saving snd file")
+
 
     @Slot(QObject)
     def loadSound(self, sound: zynthbox_sndfile):

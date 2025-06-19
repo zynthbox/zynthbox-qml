4174617c0bce64748f6f9c3d0b5cd63d219a1af2
Anupam Basak <anupam.basak27@gmail.com>
2025-06-19 12:33:33 +0530

    update_plugins_json : Update script to generate new normalized json from old json

--- config/update_plugins_json.py
+++ config/update_plugins_json.py
@@ -1,78 +1,119 @@
 import json
 
-with open("plugins.json", "r") as f:
+with open("plugins.orig.json", "r") as f:
     plugins = json.load(f)
 
-synths = plugins["synth"]
-fx = plugins["audioFx"]
+synth = plugins["synth"]
+fx = plugins["fx"]
 sf = plugins["soundfont"]
 plugins = {
     "synth": {},
-    "fx": {},
+    "audioFx": {},
     "soundfont": {}
 }
 
-index = 0
 
-for plugin_id, plugin in synths.items():
-    new_plugin_id = f"ZBP_SYNTH_{index:05d}"
-    plugins["synth"][new_plugin_id] = {
-        "displayName": plugin["name"],
+for plugin_id, plugin in synth.items():
+    plugins["synth"][plugin_id] = {
+        "displayName": plugin["displayName"],
         "description": plugin["description"],
-        "currentVersion": "0.0",
+        "longDescription": None,
+        "image": f"synths/{plugin['displayName'].lower().replace(' ', '-')}.png",
+        "categories": [
+            {
+                "type": "synth",
+                "id": "0000"
+            }
+        ],
+        "currentVersion": "0000",
         "versions": {
-            "0.0": {
-                "pluginName": plugin["name"],
-                "format": plugin["format"],
-                "path": plugin["path"],
-                "engineType": plugin["engineType"],
-                "zynthboxVersionAdded": plugin["zynthboxVersionAdded"]
+            "0000": {
+                "visible": True,
+                "pluginName": plugin["versions"]["0.0"]["pluginName"],
+                "format": plugin["versions"]["0.0"]["format"],
+                "path": plugin["versions"]["0.0"]["path"],
+                "engineType": plugin["versions"]["0.0"]["engineType"],
+                "sha256sum": None,
+                "zynthboxVersionAdded": plugin["versions"]["0.0"]["zynthboxVersionAdded"],
+                "url": plugin["versions"]["0.0"]["url"] if "url" in plugin["versions"]["0.0"] else None,
+                "volumeControls": plugin["versions"]["0.0"]["volumeControls"] if "volumeControls" in plugin["versions"]["0.0"] else None,
+                "cutoffControl": plugin["versions"]["0.0"]["cutoffControl"] if "cutoffControl" in plugin["versions"]["0.0"] else None,
+                "resonanceControl": plugin["versions"]["0.0"]["resonanceControl"] if "resonanceControl" in plugin["versions"]["0.0"] else None
             }
         }
     }
 
-    if "url" in plugin:
-        plugins["synth"][new_plugin_id]["versions"]["0.0"]["url"] = plugin["url"]
-    if "volumeControls" in plugin:
-        plugins["synth"][new_plugin_id]["versions"]["0.0"]["volumeControls"] = plugin["volumeControls"]
-    if "cutoffControl" in plugin:
-        plugins["synth"][new_plugin_id]["versions"]["0.0"]["cutoffControl"] = plugin["cutoffControl"]
-    if "resonanceControl" in plugin:
-        plugins["synth"][new_plugin_id]["versions"]["0.0"]["resonanceControl"] = plugin["resonanceControl"]
-
-    index += 1
-
-index = 0
-
-for plugin_id, plugin in fx.items():
-    new_plugin_id = f"ZBP_FX_{index:05d}"
-    plugins["fx"][new_plugin_id] = {
-        "displayName": plugin["name"],
-        "category": plugin["category"],
-        "currentVersion": "0.0",
-        "versions": {
-            "0.0": {
-                "pluginName": plugin["name"],
-                "format": plugin["format"],
-                "path": plugin["path"],
-                "engineType": plugin["engineType"],
-                "zynthboxVersionAdded": plugin["zynthboxVersionAdded"]
-            }
-        }
-    }
-
-    if "url" in plugin:
-        plugins["fx"][new_plugin_id]["versions"]["0.0"]["url"] = plugin["url"]
-
-    index += 1
-
-
-index = 0
 
 for plugin_id, plugin in sf.items():
-    new_plugin_id = f"ZBP_SF_{index:05d}"
-    plugins["soundfont"][new_plugin_id] = plugin
+    sha256sum = plugin["currentVersion"]
+    plugins["soundfont"][plugin_id] = {
+        "displayName": plugin["displayName"],
+        "description": None,
+        "longDescription": None,
+        "image": None,
+        "categories": [
+            {
+                "type": "soundfont",
+                "id": "0000"
+            }
+        ],
+        "currentVersion": "0000",
+        "versions": {
+            "0000": {
+                "visible": True,
+                "pluginName": plugin["versions"][sha256sum]["pluginName"],
+                "format": plugin["versions"][sha256sum]["format"],
+                "path": plugin["versions"][sha256sum]["path"],
+                "engineType": None,
+                "sha256sum": sha256sum,
+                "zynthboxVersionAdded": plugin["versions"][sha256sum]["zynthboxVersionAdded"],
+                "url": None,
+                "volumeControls": None,
+                "cutoffControl": None,
+                "resonanceControl": None
+            }
+        }
+    }
 
-    index += 1
+
+categoryIdByName = {}
+
+with open("categories.json", "r") as f:
+    categoriesJson = json.load(f)
+
+    for categoryId, category in categoriesJson["audioFx"].items():
+        categoryIdByName[category["displayName"]] = categoryId
+
+
+for plugin_id, plugin in fx.items():
+    plugins["audioFx"][plugin_id] = {
+        "displayName": plugin["displayName"],
+        "description": None,
+        "longDescription": None,
+        "image": None,
+        "categories": [
+            {
+                "type": "audioFx",
+                "id": categoryIdByName[plugin["category"]] if plugin["category"] in categoryIdByName else "o.O"
+            }
+        ],
+        "currentVersion": "0000",
+        "versions": {
+            "0000": {
+                "visible": True,
+                "pluginName": plugin["versions"]["0.0"]["pluginName"],
+                "format": plugin["versions"]["0.0"]["format"],
+                "path": plugin["versions"]["0.0"]["path"],
+                "engineType": plugin["versions"]["0.0"]["engineType"],
+                "sha256sum": None,
+                "zynthboxVersionAdded": plugin["versions"]["0.0"]["zynthboxVersionAdded"],
+                "url": plugin["versions"]["0.0"]["url"] if "url" in plugin["versions"]["0.0"] else None,
+                "volumeControls": plugin["versions"]["0.0"]["volumeControls"] if "volumeControls" in plugin["versions"]["0.0"] else None,
+                "cutoffControl": plugin["versions"]["0.0"]["cutoffControl"] if "cutoffControl" in plugin["versions"]["0.0"] else None,
+                "resonanceControl": plugin["versions"]["0.0"]["resonanceControl"] if "resonanceControl" in plugin["versions"]["0.0"] else None
+            }
+        }
+    }
+
 
 print(json.dumps(plugins, indent=2))
